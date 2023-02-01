d3b36b658496ecd4334e9c0a003e3f1099e3ddbd
Anja Wurlitzer <anja.wurlitzer.coding@gmail.com>
2023-02-01 17:25:44 +0100

    refactor: add restful API instead local stage

--- script.js
+++ script.js
@@ -1,73 +1,41 @@
-const state = {
-  todos: [{ description: "Put your Todos here", done: false, id: "0" }],
-};
-console.log(state);
+let todos = [];
 
-// Local Storage
-function updateLocalStorage() {
-  const userUptodo = localStorage.setItem(
-    "userUptodo",
-    JSON.stringify(state.todos)
-  );
-}
-function loadLocalStorage() {
-  if (
-    localStorage.getItem("userUptodo") !== "[]" &&
-    localStorage.getItem("userUptodo")
-  ) {
-    //Local storage file is there and is not empty (after deleting all entries)
-    const userDownToDo = JSON.parse(localStorage.getItem("userUptodo"));
-    state.todos = userDownToDo;
-    console.log(localStorage.getItem("userUptodo"));
-  } else {
-    updateLocalStorage();
-  }
-}
+fetch("http://localhost:4730/todos")
+  .then((response) => response.json())
+  .then((data) => {
+    todos = data;
+    console.log(todos);
+    data.forEach(render);
+  });
 
 //render entries:
-function render() {
-  document.querySelector("#todo-list").innerHTML = "";
-  state.todos.forEach((todo) => {
-    let liEl = document.createElement("li");
-    let inputEl = document.createElement("input");
-    let labelEl = document.createElement("label");
+function render(todo) {
+  let liEl = document.createElement("li");
+  let inputEl = document.createElement("input");
+  let labelEl = document.createElement("label");
 
-    liEl.setAttribute("class", `${todo.id} yours`);
-    inputEl.setAttribute("type", "checkbox");
-    inputEl.setAttribute("class", "todo");
-    inputEl.setAttribute("id", todo.id);
-    labelEl.setAttribute("for", todo.id);
+  liEl.setAttribute("class", `${todo.id} yours`);
+  inputEl.setAttribute("type", "checkbox");
+  inputEl.setAttribute("class", "todo");
+  inputEl.setAttribute("id", todo.id);
+  labelEl.setAttribute("for", todo.id);
 
-    liEl.append(inputEl, labelEl);
-    labelEl.appendChild(document.createTextNode(todo.description));
-    document.querySelector("#todo-list").appendChild(liEl);
+  liEl.append(inputEl, labelEl);
+  labelEl.appendChild(document.createTextNode(todo.description));
+  document.querySelector("#todo-list").appendChild(liEl);
 
-    if (todo.done === true) {
-      inputEl.setAttribute("checked", "true");
-    }
-    updateLocalStorage();
-  });
+  if (todo.done === true) {
+    inputEl.setAttribute("checked", "true");
+  }
 }
-
-//init:
-loadLocalStorage();
-render();
 
 //add new entry:
 function addTodo(event) {
   event.preventDefault();
   let x = document.querySelector("#user-input").value;
-  let y;
-
-  if (!state.todos[0]) {
-    //if no entry in state
-    y = 1;
-  } else {
-    y = +state.todos[state.todos.length - 1].id + 1;
-  }
 
   try {
-    state.todos.forEach((todo) => {
+    todos.forEach((todo) => {
       //for doublicated entries:
       if (x.toLowerCase() === todo.description.toLowerCase()) {
         throw new Error("double");
@@ -75,13 +43,21 @@ function addTodo(event) {
     });
 
     if (x.length >= 5) {
-      //state={todos:[{},{},...]}
-      state.todos.push({ description: `${x}`, done: `false`, id: `${y}` });
-
-      render();
+      const newTodo = { description: x, done: false };
+      fetch("http://localhost:4730/todos", {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(newTodo),
+      })
+        .then((response) => response.json())
+        .then((newTodoFromApi) => {
+          render(newTodoFromApi);
+          todos.push(newTodoFromApi);
+          console.log(todos);
+        });
 
       document.querySelector("#user-input").removeAttribute("style");
-      event.target.reset();
+      event.target.reset(); //empty the input element
     } else {
       //for entries shorter than 5 characters
       throw new Error("short");
@@ -111,22 +87,43 @@ document.querySelector("input").addEventListener("keydown", (event) => {
 
 //mark as done:
 document.querySelector("#todo-list").addEventListener("change", (event) => {
-  state.todos.forEach((todo) => {
-    if (todo.id === event.target.id) {
+  let descr;
+  for (let todo of todos) {
+    if (event.target.id === `${todo.id}`) {
+      descr = todo.description;
       todo.done = event.target.checked;
-      updateLocalStorage();
     }
-  });
+  }
+  const changedTodo = {
+    description: descr,
+    done: event.target.checked,
+    id: event.target.id,
+  };
+  fetch(`http://localhost:4730/todos/${event.target.id}`, {
+    method: "PUT",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify(changedTodo),
+  })
+    .then((response) => response.json())
+    .then((newTodoFromApi) => {
+      console.log(newTodoFromApi);
+    });
 });
 
 //remove done:
 document.querySelector("#remove").addEventListener("click", () => {
-  for (let i = state.todos.length - 1; i >= 0; i--) {
-    if (state.todos[i].done === true) {
-      state.todos.splice(i, 1);
+  for (let i = todos.length - 1; i >= 0; i--) {
+    if (todos[i].done === true) {
+      fetch(`http://localhost:4730/todos/${todos[i].id}`, {
+        method: "DELETE",
+      })
+        .then((response) => response.json())
+        .then();
+      todos.splice(i, 1);
     }
   }
-  render();
+  document.querySelector("#todo-list").innerHTML = "";
+  todos.forEach(render);
 });
 
 //filter:
