fedb773684e9e93e392e88a315e9602234e5e5e8
Anja Wurlitzer <anja.wurlitzer.coding@gmail.com>
2023-02-09 12:25:39 +0100

    refactor: tests dont depend on eachother anymore

--- cypress/e2e/todo.cy.js
+++ cypress/e2e/todo.cy.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress"/>
 
-/*give all elements to test a data-cy
+/*give all testing elements a data-cy
 
 -input field = "input-shit"
 -add button = "add-button"
@@ -21,26 +21,6 @@ describe("Todo App", () => {
     cy.get("[data-cy='input-shit']").should("exist");
   });
 
-  //input of new todo:
-  it("should add new elements", () => {
-    const typeIn = "learn cypress";
-    cy.get("[data-cy='input-shit']").type(typeIn);
-    cy.get("[data-cy='add-button']").click();
-    cy.get(`[data-cy='${typeIn}']`).should("exist");
-    cy.get(`[data-cy='${typeIn}']`)
-      .find("[data-cy='checkbox']")
-      .should("be.enabled");
-  });
-
-  //duplication check
-  it("should not allow duplicated entries", () => {
-    const typeIn = "Learn HTML";
-    cy.get("[data-cy='input-shit']").type(typeIn);
-    cy.get("[data-cy='add-button']").click();
-    Cypress.on("uncaught:exception", () => false);
-    cy.get(`[data-cy='${typeIn}']`).should("have.length", 1);
-  });
-
   //check if not shorter than 5 characters
   it("should not allow entries with less than 5 characters", () => {
     const typeIn = "hi";
@@ -50,39 +30,80 @@ describe("Todo App", () => {
     cy.get(`[data-cy='${typeIn}']`).should("not.exist");
   });
 
-  context("with a checked task", () => {
+  context("with a new task", () => {
     beforeEach(() => {
       const typeIn = "learn cypress";
-      cy.get(`[data-cy='${typeIn}']`).find("[data-cy='checkbox']").check();
+      cy.get("[data-cy='input-shit']").type(typeIn);
+      cy.get("[data-cy='add-button']").click();
     });
 
-    //check element is done ??????
-    it("should mark elements as done", () => {
+    afterEach(() => {
       const typeIn = "learn cypress";
-      cy.get(`[data-cy='${typeIn}']`)
-        .find("[data-cy='checkbox']")
-        .should("be.enabled");
-    });
-
-    //filter
-    it("can filter uncompleted tasks", () => {
-      cy.get("[data-cy='open']").click();
-      cy.get("[data-cy='checkbox']:checked")
-        .parents()
-        .should("have.css", "display", "none");
-    });
-
-    it("can filter completed tasks", () => {
-      cy.get("[data-cy='done']").click();
-      cy.get("[data-cy='checkbox']:not(:checked)")
-        .parents()
-        .should("have.css", "display", "none");
-    });
-
-    //deletion
-    it("can delete done todos", () => {
+      cy.get(`[data-cy='${typeIn}']`).find("[data-cy='checkbox']").check();
       cy.get("[data-cy='remove-button']").click();
-      cy.get("[data-cy='checkbox']:checked").should("not.exist");
     });
+
+    //input of new todo:
+    it("should add new elements", () => {
+      const typeIn = "learn cypress";
+      cy.get(`[data-cy='${typeIn}']`).should("exist");
+    });
+
+    //duplication check
+    it("should not allow duplicated entries", () => {
+      const typeIn = "learn cypress";
+      cy.get("[data-cy='input-shit']").type(typeIn);
+      cy.get("[data-cy='add-button']").click();
+      Cypress.on("uncaught:exception", () => false);
+      cy.get(`[data-cy='${typeIn}']`).should("have.length", 1);
+    });
+
+    context("with a checked task", () => {
+      beforeEach(() => {
+        const typeIn = "learn cypress";
+        cy.get(`[data-cy='${typeIn}']`).click();
+      });
+
+      afterEach(() => {
+        cy.get("[data-cy='all']").click();
+        const typeIn = "learn cypress";
+        cy.get(`[data-cy='${typeIn}']`).click();
+      });
+
+      //check element is done
+      it("should mark elements as done", () => {
+        const typeIn = "learn cypress";
+        cy.get(`[data-cy='${typeIn}']`)
+          .find("[data-cy='checkbox']:checked")
+          .should("exist");
+      });
+
+      //filter
+      it("can filter uncompleted tasks", () => {
+        cy.get("[data-cy='open']").click();
+        cy.get("[data-cy='checkbox']:checked")
+          .parents()
+          .should("have.css", "display", "none");
+      });
+
+      it("can filter completed tasks", () => {
+        cy.get("[data-cy='done']").click();
+        cy.get("[data-cy='checkbox']:not(:checked)")
+          .parents()
+          .should("have.css", "display", "none");
+      });
+    });
+  });
+
+  //deletion
+  it("can delete done todos", () => {
+    const typeIn = "learn cypress";
+    cy.get("[data-cy='input-shit']").type(typeIn);
+    cy.get("[data-cy='add-button']").click();
+
+    cy.get(`[data-cy='${typeIn}']`).click();
+
+    cy.get("[data-cy='remove-button']").click();
+    cy.get("[data-cy='checkbox']:checked").should("not.exist");
   });
 });
